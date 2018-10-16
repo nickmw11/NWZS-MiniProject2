e13b4581e330cad647286891e7f0fffff9bf1939
Zac <zachary.spate@stvincent.edu>
2018-10-15 22:22:13 -0400

    Init of base project

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour {
+
+    public string playerPrefHighScoreKey = "playerHighScore";
+    static public Constants S;
+    public int highScore;
+
+    void Awake()
+    {
+        S = this;
+
+        //if (!PlayerPrefs.HasKey(playerPrefHighScoreKey))
+        //{
+        //    highScore = PlayerPrefs.GetInt(playerPrefHighScoreKey);
+        //    Debug.Log("Reading Player Prefs");
+        //}
+        //else
+        //{
+        //    Debug.Log("setting player prefs for first time");
+        //    PlayerPrefs.SetInt(playerPrefHighScoreKey, highScore);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(playerPrefHighScoreKey))
+        {
+            highScore = PlayerPrefs.GetInt(playerPrefHighScoreKey);
+            Debug.Log("Reading Player Prefs");
+        }
+        else
+        {
+            Debug.Log("setting player prefs for first time");
+            PlayerPrefs.SetInt(playerPrefHighScoreKey, highScore);
+        }
+    }
+}
---
+++ Assets/Scripts/SplashScreen/SplashSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashSceneManager : MonoBehaviour {
+
+    public float time;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time += Time.deltaTime;
+        //if (time > 1)
+        //    UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(3f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+    }
+}
